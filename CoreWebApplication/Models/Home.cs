d5ee144518630d2b1837c75b41a4b83dad4b81cc
Muhammad Nouman Arif <nomirana442@outlook.com>
2023-04-06 22:19:57 +0500

    commit core app

---
+++ CoreWebApplication/Models/Home.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreWebApplication.Models
+{
+    public class Home
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Url { get; set; }
+        public Home() { }
+
+    }
+}
